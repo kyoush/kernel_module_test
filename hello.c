e289c673a5e536ecb1f9b9ed8816cec518872c42
Kyoshiro Kamura <reallove1970@gmail.com>
2025-09-06 19:34:17 +0900

    hello

---
+++ hello.c
@@ -0,0 +1,20 @@
+#include <linux/modules.h>
+
+static int __init hello_init(void)
+{
+	pr_info("Hello World!\n");
+	return 0;
+}
+
+static void __exit hello_exit(void)
+{
+	pr_info("Goodbye world\n");
+}
+
+modules_init(hello_init);
+modules_exit(hello_exit);
+
+MODULE_LICENSE("GPL v2");
+MODULE_AUTHOR("Kyoshiro Kamura <kyoshi@kamura.email>");
+MODULE_DESCRIPTION("A simple Hello World Linux kernel module");
+
